16871a0574f257e125c586004346f53d19fa8384
Dillon Hamman <dillonhamman@Dillons-MacBook-Pro.local>
2020-08-31 13:39:44 -0700

    fix for loop

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -7,8 +7,11 @@ output: 3
 
 var median = function(input) {
     var total = 0;
-    for (i = 0; i < input.length - 1; i ++){
+    for (i = 0; i < input.length; i ++){
         total += input[i];
+        console.log(total, input[i]);
     }
     return total / input.length;
 };
+
+console.log(median([1, 2, 2, 3, 4, 7, 9]));
