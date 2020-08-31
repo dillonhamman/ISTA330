fa5bc44813a7d7cef321ba11e8e986d0c6aa4362
Dillon Hamman <dillonhamman@Dillons-MacBook-Pro.local>
2020-08-31 13:01:31 -0700

    Q3 using nested for loop and a push statement

--- practicum/week-1/question3.js
+++ practicum/week-1/question3.js
@@ -9,5 +9,17 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
-
+    var orderedPairs = [];
+    for (i = 0; i <= input.length - 1; i ++){
+        for (j = 0; j <= input.length - 1; j ++){
+            if (input[i] < input[j]){
+                if (i <= j){
+                    orderedPairs.push([input[i], input[j]]);
+                }
+            }
+        }
+    }
+    
+    return orderedPairs.length;
 };
+
