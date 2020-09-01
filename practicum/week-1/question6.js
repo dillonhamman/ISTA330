36337499bf6c017baac85aa4d8570bcea0d91fe6
Dillon Hamman <dillonhamman@Dillons-MacBook-Pro.local>
2020-09-01 10:04:11 -0700

    using math.floor()

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,11 +6,8 @@ output: 3
 */
 
 var median = function(input) {
-    var total = 0;
-    for (i = 0; i < input.length; i ++){
-        total += input[i];
-        console.log(total, input[i]);
-    }
-    return total / input.length;
+    var m = Math.floor(input.length / 2);
+    return input[m];
 };
 
+console.log(median([1, 2, 3, 4, 5, 6, 7, 8]));
