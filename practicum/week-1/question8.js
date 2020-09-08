118dd5f135e336626fe6d4c59baf564e6e89e47c
Dillon Hamman <dillonhamman@Dillons-MacBook-Pro.local>
2020-09-08 13:42:10 -0700

    nested for loop

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -8,5 +8,18 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
-
+    var outList = [];
+    for (i = 0; i < input.length; i ++){
+        curr = input[i];
+        count = 0;
+        for (j = 0; j < input.length; j ++){
+            if ( input[j] > curr && input[j] % 2 == 0){
+                count ++; 
+            }
+        }
+        outList.push(count);
+    }
+    return outList;
 };
+
+console.log(biggerAndEven([23, 44, 12 ,4]));
